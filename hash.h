063dcc9f1b3021735fb934457eab2efa5f8a6268
Sam Queen <samqueenbusiness@gmail.com>
2022-07-23 07:16:27 -0700

    fixed merge problem

--- hash.h
+++ hash.h
@@ -5,10 +5,4 @@
 
 using namespace std;
 
-<<<<<<< HEAD
 int hash_function(string text, int k);
-=======
-int hash_function(string text);
-
-public void printHash();
->>>>>>> 1303277bd5a8cbdc9256728e5f3ac750861be41a
